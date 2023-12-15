8e1858efe4ab22f6d4ed03bfd7c57681d4f943a3
Mario Garcia <themariogarcia@gmail.com>
2023-12-15 12:20:34 -0600

    added and completed the actor / person screens which displays actor details

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { HomeScreen } from '../screens/HomeScreen'
 import { MovieScreen } from '../screens/MovieScreen'
+import { PersonScreen } from '../screens/PersonScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -19,6 +20,11 @@ export default AppNavigation = () => {
           name="Movie"
           options={{ headerShown: false }}
           component={MovieScreen}
+        />
+        <Stack.Screen
+          name="Person"
+          options={{ headerShown: false }}
+          component={PersonScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
